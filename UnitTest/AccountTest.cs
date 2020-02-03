8c75644f26b426a2933855aa512b5ba160662954
Dominik Palenik <domuss574@gmail.com>
2020-02-03 20:57:21 +0100

    Added unit tests

---
+++ UnitTest/AccountTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using MemeApp;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void LoadDataFromFile_FileDontExists_CreateNewFile()
+        {
+            if (File.Exists("AccountData.txt")) File.Delete("AccountData.txt");
+            Account.LoadDataFromFile();
+
+            Assert.IsTrue(File.Exists("AccountData.txt"));
+
+            if(File.Exists("AccountData.txt"))
+            {
+                string[] data = new string[2];
+
+                data = File.ReadAllLines("AccountData.txt");
+                string username = data[0];
+                string password = data[1];
+
+                Assert.AreEqual(username, "Guest");
+                Assert.AreEqual(Account.username, "Guest");
+                Assert.AreEqual(password, "");
+                Assert.AreEqual(Account.password, "");
+            }
+        }
+
+        [TestMethod]
+        public void LoadDataFromFile_FileIsEmpty_CreateNewFile()
+        {
+            File.WriteAllText("AccountData.txt", "");
+
+            Account.LoadDataFromFile();
+
+            Assert.IsTrue(File.Exists("AccountData.txt"));
+
+            if (File.Exists("AccountData.txt"))
+            {
+                string[] data = new string[2];
+
+                data = File.ReadAllLines("AccountData.txt");
+                string username = data[0];
+                string password = data[1];
+
+                Assert.AreEqual(username, "Guest");
+                Assert.AreEqual(Account.username, "Guest");
+                Assert.AreEqual(password, "");
+                Assert.AreEqual(Account.password, "");
+            }
+        }
+
+        [TestMethod]
+        public void LoadDataFromFile_IncorrectData_CreateNewFile()
+        {
+            string text = "Guest" + Environment.NewLine
+                + "asfdsfsdf" + Environment.NewLine;
+            File.WriteAllText("AccountData.txt", text);
+
+            Account.LoadDataFromFile();
+
+            Assert.IsTrue(File.Exists("AccountData.txt"));
+
+            if (File.Exists("AccountData.txt"))
+            {
+                string[] data = new string[2];
+
+                data = File.ReadAllLines("AccountData.txt");
+                string username = data[0];
+                string password = data[1];
+
+                Assert.AreEqual(username, "Guest");
+                Assert.AreEqual(Account.username, "Guest");
+                Assert.AreEqual(password, "");
+                Assert.AreEqual(Account.password, "");
+            }
+        }
+
+        [TestMethod]
+        public void CreateAccountDataFile_NormalInput_CreatesAccountDataFileWithProvidedUsernameAndPassword()
+        {
+            Account.CreateAccountDataFile("Adam", "asd");
+            Assert.IsTrue(File.Exists("AccountData.txt"));
+
+            if (File.Exists("AccountData.txt"))
+            {
+                string[] data = new string[2];
+
+                data = File.ReadAllLines("AccountData.txt");
+                string username = data[0];
+                string password = data[1];
+
+                Assert.AreEqual(username, "Adam");
+                Assert.AreEqual(Account.username, "Adam");
+                Assert.AreEqual(password, "asd");
+                Assert.AreEqual(Account.password, "asd");
+            }
+        }
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("259e29f4-e5e5-48e1-99d5-5b44b3c46c48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
